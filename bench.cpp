0a16de326d125facb3145824768aa821ca2f435c
Sam <samuelrcain@gmail.com>
2022-08-13 22:29:00 -0500

    Added VECTOR flag to test performance between pointer and vector

---
+++ bench.cpp
@@ -0,0 +1,33 @@
+#include <chrono>
+#include <iostream>
+#include "Capture.h"
+
+int main() {
+
+    ScreenCapture screen(RES_1080);
+
+    PixelData image = nullptr;
+
+    std::chrono::steady_clock::time_point start, end;
+    long long avgDuration = 0;
+
+    int trials = 100;
+    for (int trial = 0; trial < trials; trial++) {
+
+        start = std::chrono::high_resolution_clock::now();
+
+        screen.CaptureScreen();
+        screen.GetImageData(image);
+
+        end = std::chrono::high_resolution_clock::now();
+
+        avgDuration += std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    }
+
+    avgDuration /= trials;
+
+    std::cout << avgDuration << " microseconds\n";
+
+    screen.SaveToFile();
+
+}
